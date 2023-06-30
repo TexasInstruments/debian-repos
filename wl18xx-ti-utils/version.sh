257bd17f4bf3afef8963ec25f016524f7957013a
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-06-30 05:24:41 +0000

    wl18xx-ti-utils: add debain/ and version.sh file for "wlconf"
    Signed-off-by: Suhaas Joshi <joshiesuhaas0@gmail.com>

---
+++ wl18xx-ti-utils/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export git_repo="https://git.ti.com/git/wilink8-wlan/18xx-ti-utils.git"
+export package_name="wl18xx-ti-utils"
+export package_version="0.8"
+export package_full="${package_name}-${package_version}"
+export custom_build=false
+export require_root=false
+
+function run_prep() {
+    cp -rv "${projdir}/suite/bookworm/debian/patches" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/rules" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/copyright" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/wl18xx-ti-utils.manpages" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/wlconf.1" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/calibrator.1" "${builddir}/${package_full}/debian/"
+    cp -rv "${projdir}/suite/bookworm/debian/source" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/changelog" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/control" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/wl18xx-ti-utils.lintian-overrides" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/watch" "${builddir}/${package_full}/debian/"
+    cp -v "${projdir}/suite/bookworm/debian/README.Debian" "${builddir}/${package_full}/debian/"
+}
+
