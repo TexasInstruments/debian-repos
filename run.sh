03ce81f0057b64a91fae48b56de27d1616e24751
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-06-30 04:53:41 +0000

    run: write top-level script to run debian packaging process
    Write top-level script to run Debain packaging process. The script refers to
    variables and functions defined in <package-name>/version.sh file to carry out
    package-specific configurations and operations.
    
    The sub-directories in the source directory are the source package names of TI's
    packages. When an argument is passed to the script, it looks for a
    sub-directory in the top-level directory with a matching name. Thus all
    sub-directories for packages must have the same name as the source package of
    that project.
    
    Signed-off-by: Suhaas Joshi <joshiesuhaas0@gmail.com>

---
+++ run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "run.sh: missing operands"
+    echo "Requires source package name as argument"
+    exit 1
+fi
+
+topdir=$(git rev-parse --show-toplevel)
+projdir="${topdir}/$1"
+builddir="${topdir}/build/$1"
+sourcedir="${builddir}/sources"
+
+if [ ! -d ${projdir} ]; then
+    echo "This project does not exist."
+    echo "Exiting."
+    exit 1
+fi
+
+mkdir -p "${sourcedir}"
+
+source ${projdir}/version.sh
+
+if $custom_build ; then
+    run_custom_build
+    exit 0
+fi
+
+if [ $require_root ] && [ "$EUID" -ne 0 ] ; then
+    echo "Requires root privileges to execute"
+    echo "Exiting"
+    exit 1
+fi
+
+cd ${builddir}
+if [ ! -f "${package_full}.tar.gz" ]; then
+    cd "${sourcedir}"
+    if [ ! -d "${package_full}" ]; then
+        git clone "${git_repo}" "${package_full}"
+    fi
+    tar -cvzf "${builddir}/${package_full}.tar.gz" "${package_full}"
+    cd ${builddir}
+    tar -xzmf "${package_full}.tar.gz"
+fi
+
+cd "${builddir}/${package_full}"
+if [ ! -d "debian" ]; then
+	debmake
+fi
+
+run_prep
+
+# Apply un-applied Debian patches
+patch_dir="${builddir}/${package_full}/debian/patches"
+while read -r patch; do
+    if [ -f "${builddir}/series" ]; then
+        if grep -Fxq "$patch" "${builddir}/series" ; then
+            continue
+        fi
+    fi
+    git apply --check "${patch_dir}/${patch}"
+done < "${patch_dir}/series"
+cp "${patch_dir}/series" "${builddir}"
+
+debuild --no-lintian
