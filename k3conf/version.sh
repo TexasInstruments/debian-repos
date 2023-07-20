55b4d1e43ccca8b8a328751c392f32e7345bf171
Andrew Davis <afd@ti.com>
2023-07-20 11:24:11 -0500

    k3conf: Update to latest upstream v0.2.53
    The latest upstream removes the need for the patch, drop that. We also
    fix the version number, the dash in the version is for changes made when
    debianizing, as this is a clean package using that confuses the debian
    tools when looking for the orig source.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- k3conf/version.sh
+++ k3conf/version.sh
@@ -2,14 +2,13 @@
 
 export git_repo="https://git.ti.com/git/k3conf/k3conf.git"
 export package_name="k3conf"
-export package_version="0.1-90"
+export package_version="0.2.53"
 export package_full="${package_name}-${package_version}"
 export custom_build=false
 export require_root=false
-export last_tested_commit="1dd468d551fd786c410e88dadc1114505d057ebe"
+export last_tested_commit="81581afb405085755aea7744c1d196533e8094c4"
 
 function run_prep() {
-    mv "${builddir}/k3conf-0.1_90.orig.tar.gz" "${builddir}/k3conf_0.1.orig.tar.gz"
     cp -rv "${projdir}/suite/bookworm/debian/patches" "${builddir}/${package_full}/debian/"
     cp -v "${projdir}/suite/bookworm/debian/rules" "${builddir}/${package_full}/debian/"
     cp -v "${projdir}/suite/bookworm/debian/copyright" "${builddir}/${package_full}/debian/"
