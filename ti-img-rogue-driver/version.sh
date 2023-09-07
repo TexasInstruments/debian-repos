84ebb28c79e93a5fefd850036b5fc6d2123b0994
Andrew Davis <afd@ti.com>
2023-09-07 12:13:19 -0500

    ti-img-rogue-driver: Add missing semicolon to DKMS check
    Signed-off-by: Andrew Davis <afd@ti.com>

--- ti-img-rogue-driver/version.sh
+++ ti-img-rogue-driver/version.sh
@@ -11,7 +11,7 @@ export last_tested_commit="ebddb087ef140ca83e4c30d66580b0bb33b003fd"
 function run_prep() {
     rm "${builddir}/${package_full}/debian/README.Debian"
     
-    if ! command -v dkms &>/dev/null then
+    if ! command -v dkms &>/dev/null ; then
         echo "dkms not found: ensure that it is installed"
         exit 1
     fi
