0caea3c88fd70cfb5da716790332648b5814368b
Andrew Davis <afd@ti.com>
2023-09-07 12:11:04 -0500

    run: Copy Debian control files automatically
    Only files we want to deploy should be in the debian/ dir,
    copy everything automatically. Remove the manual copies from
    the project run_prep functions.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- ti-rpmsg-char/version.sh
+++ ti-rpmsg-char/version.sh
@@ -7,17 +7,3 @@ export package_full="${package_name}-${package_version}"
 export custom_build=false
 export require_root=false
 export last_tested_commit="510e90d0a75bc21d79020669d2ea03f8222f3b8d"
-
-function run_prep() {
-    cp -rv "${projdir}/suite/bookworm/debian/patches" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/rules" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/copyright" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/libti-rpmsg-char-dev.install" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/libti-rpmsg-char.install" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/libti-rpmsg-char-dev.lintian-overrides" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/libti-rpmsg-char.lintian-overrides" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/watch" "${builddir}/${package_full}/debian/"
-    cp -rv "${projdir}/suite/bookworm/debian/source" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/changelog" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/control" "${builddir}/${package_full}/debian/"
-}
--- wl18xx-ti-utils/version.sh
+++ wl18xx-ti-utils/version.sh
@@ -7,19 +7,3 @@ export package_full="${package_name}-${package_version}"
 export custom_build=false
 export require_root=false
 export last_tested_commit="7325bf0b7b2d462e334437d2c7f9198d0ac55ce2"
-
-function run_prep() {
-    cp -rv "${projdir}/suite/bookworm/debian/patches" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/rules" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/copyright" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/wl18xx-ti-utils.manpages" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/wlconf.1" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/calibrator.1" "${builddir}/${package_full}/debian/"
-    cp -rv "${projdir}/suite/bookworm/debian/source" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/changelog" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/control" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/wl18xx-ti-utils.lintian-overrides" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/watch" "${builddir}/${package_full}/debian/"
-    cp -v "${projdir}/suite/bookworm/debian/README.Debian" "${builddir}/${package_full}/debian/"
-}
-
