13d6a24f534dbd99684f4229ef90142ae7334ba8
Andrew Davis <afd@ti.com>
2023-09-18 13:25:05 -0500

    run: Enable git SHA to be deduced from changelog file
    This allows the SHA to be found automatically and kept in sync
    with the changelog version. It also means we only need to change
    one spot to bump git versions.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- run.sh
+++ run.sh
@@ -25,6 +25,7 @@ fi
 package_name=$(cd ${debcontroldir} && dpkg-parsechangelog --show-field Source)
 deb_version=$(cd ${debcontroldir} && dpkg-parsechangelog --show-field Version)
 package_version=$(echo $deb_version | cut -d'-' -f1)
+last_tested_commit=$(echo $package_version | sed 's/.*+//')
 package_full="${package_name}-${package_version}"
 package_full_ll="${package_name}_${package_version}"
 echo "Building " $package_name " version " $deb_version
