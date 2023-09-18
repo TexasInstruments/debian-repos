8eeb4bb623552e95d8da2385cc232834b5ab55b8
Andrew Davis <afd@ti.com>
2023-09-18 13:27:13 -0500

    k3conf: Update bookworm to version 0.2+git20230830+859e1cc-1
    Signed-off-by: Andrew Davis <afd@ti.com>

--- k3conf/version.sh
+++ k3conf/version.sh
@@ -3,4 +3,3 @@
 export git_repo="https://git.ti.com/git/k3conf/k3conf.git"
 export custom_build=false
 export require_root=false
-export last_tested_commit="81581afb405085755aea7744c1d196533e8094c4"
