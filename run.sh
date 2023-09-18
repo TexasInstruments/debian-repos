b15e51b49d3a2a2819796db4dc2ec300584d130b
Andrew Davis <afd@ti.com>
2023-09-18 12:47:36 -0500

    run: Remove remaining directory changes
    Changing directory before commands can lead to later commands depending
    on a given directory location. It also makes it more difficult to reorder
    commands or change our directory structure. Switch to specifying the
    directory in commands that support it, change directory in a sub-shell
    for all others
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- run.sh
+++ run.sh
@@ -17,14 +17,12 @@ if [ ! -d ${projdir} ]; then
     exit 1
 fi
 
-cd ${projdir}/suite/bookworm/
-package_name=$(dpkg-parsechangelog --show-field Source)
-deb_version=$(dpkg-parsechangelog --show-field Version)
+package_name=$(cd ${projdir}/suite/bookworm/ && dpkg-parsechangelog --show-field Source)
+deb_version=$(cd ${projdir}/suite/bookworm/ && dpkg-parsechangelog --show-field Version)
 package_version=$(echo $deb_version | cut -d'-' -f1)
 package_full="${package_name}-${package_version}"
 package_full_ll="${package_name}_${package_version}"
 echo "Building " $package_name " version " $deb_version
-cd -
 
 source ${projdir}/version.sh
 
@@ -43,8 +41,8 @@ fi
 
 mkdir -p "${sourcedir}"
 
-cd ${builddir}
-if [ ! -f "${package_full_ll}.orig.tar.gz" ]; then
+# Generate original source tarball if none found
+if [ ! -f "${builddir}/${package_full_ll}.orig.tar.gz" ]; then
     if [ ! -d "${sourcedir}/${package_name}" ]; then
         git clone "${git_repo}" "${sourcedir}/${package_name}"
     fi
@@ -54,11 +52,12 @@ if [ ! -f "${package_full_ll}.orig.tar.gz" ]; then
       --transform "s,${sourcedir}/${package_name},${package_full},"
 fi
 
-tar -xzmf "${package_full_ll}.orig.tar.gz"
+# Extract original source tarball
+tar -xzmf "${builddir}/${package_full_ll}.orig.tar.gz" -C "${builddir}"
 
-cd "${builddir}/${package_full}"
-if [ ! -d "debian" ]; then
-	debmake
+# Add default debian control files if none found
+if [ ! -d "${builddir}/${package_full}/debian" ]; then
+    (cd "${builddir}/${package_full}" && debmake)
 fi
 
 # Deploy our Debian control files
@@ -67,14 +66,12 @@ cp -rv "${projdir}/suite/bookworm/debian" "${builddir}/${package_full}/"
 run_prep
 
 # Build source package
-dpkg-source -b .
+(cd "${builddir}/${package_full}" && dpkg-source -b .)
 
 # Extract source package
-cd "${builddir}"
-if [ ! -d "${package_name}_${deb_version}" ]; then
-    dpkg-source -x "${package_name}_${deb_version}.dsc" "${package_name}_${deb_version}"
+if [ ! -d "${builddir}/${package_name}_${deb_version}" ]; then
+    dpkg-source -x "${builddir}/${package_name}_${deb_version}.dsc" "${builddir}/${package_name}_${deb_version}"
 fi
 
 # Build binary package
-cd "${package_name}_${deb_version}"
-debuild --no-lintian
+(cd "${builddir}/${package_name}_${deb_version}" && debuild --no-lintian)
