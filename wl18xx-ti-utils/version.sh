2d064026c8f5fb627dc7081d0fbdf4f476dc9ae1
Sai Sree Kartheek Adivi <sskartheek.adivi@outlook.com>
2023-12-11 02:38:19 +0530

    wl18xx-ti-utils: Update versioning and start from scratch
    Signed-off-by: Sai Sree Kartheek Adivi <sskartheek.adivi@outlook.com>

--- wl18xx-ti-utils/version.sh
+++ wl18xx-ti-utils/version.sh
@@ -3,4 +3,3 @@
 export git_repo="https://git.ti.com/git/wilink8-wlan/18xx-ti-utils.git"
 export custom_build=false
 export require_root=false
-export last_tested_commit="7325bf0b7b2d462e334437d2c7f9198d0ac55ce2"
