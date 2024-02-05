a204d7e462b2cf84f256abf49c6d10fd6744dcec
Andrew Davis <afd@ti.com>
2024-02-05 17:17:44 -0600

    ti-linux-kernel: Build ti-linux-kernel like a normal package
    Signed-off-by: Andrew Davis <afd@ti.com>

--- ti-linux-kernel/version.sh
+++ ti-linux-kernel/version.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
 export git_repo="https://git.ti.com/git/ti-linux-kernel/ti-linux-kernel.git"
-export custom_build=true
+export custom_build=false
 export require_root=false
-export release_tag="09.02.00.009"
-export package_name="linux-upstream"
-
-export DEBFULLNAME="Sai Sree Kartheek Adivi"
-export DEBEMAIL="s-adivi@ti.com"
-export KDEB_CHANGELOG_DIST=bookworm
-
-function run_custom_build() {
-    cd "${builddir}"
-    if [ ! -d ${package_name} ]; then
-        git clone "${git_repo}" -b "${release_tag}" --single-branch --depth=1 ${package_name}
-    fi
-
-    cd ${package_name}
-
-    if [ ! -f ".config" ]; then
-        make -j $((`nproc`-2)) defconfig ti_arm64_prune.config
-
-    fi
-
-    make -j $((`nproc`-2)) bindeb-pkg LOCALVERSION=-k3
-}
