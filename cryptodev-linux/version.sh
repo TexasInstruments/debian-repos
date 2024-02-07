13ca352ca568cf83a3c669be0af1b6a433f7ef3f
Andrew Davis <afd@ti.com>
2024-02-07 13:30:35 -0600

    cryptodev-linux: Remove build dependency check
    Build dependencies are installed before build, no need to check
    for this explicitly.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- cryptodev-linux/version.sh
+++ cryptodev-linux/version.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
 export git_repo="https://github.com/cryptodev-linux/cryptodev-linux.git"
-
-function run_prep() {
-    if ! command -v dkms &>/dev/null ; then
-        echo "dkms not found: ensure that it is installed"
-        exit 1
-    fi
-}
