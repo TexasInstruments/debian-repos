540e55b752f4c050f21d917b8e68df67351ebc5b
Andrew Davis <afd@ti.com>
2024-03-06 09:01:43 -0600

    run: Do not generate debian directory using debmake
    The debmake tool is useful for bootstrapping non-debianized projects
    by generating some default debian files. This is not useful for the
    projects in this repo which all have debian files already. Remove
    this step.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- run.sh
+++ run.sh
@@ -53,11 +53,6 @@ if [ ! -f "${builddir}/${package_name}_${deb_version}.dsc" ]; then
     # Extract original source tarball
     tar -xzmf "${builddir}/${package_full_ll}.orig.tar.gz" -C "${builddir}"
 
-    # Add default debian control files if none found
-    if [ ! -d "${builddir}/${package_full}/debian" ]; then
-        (cd "${builddir}/${package_full}" && debmake)
-    fi
-
     # Deploy our Debian control files
     cp -rv "${debcontroldir}/debian" "${builddir}/${package_full}/"
 
